d4c39fa29a8ded8775e1fca7c973c3ed8dce8b27
ElNefro <Yanik0206@gmail.com>
2024-10-25 18:35:08 +0500

    jr

--- Deck.hpp
+++ Deck.hpp
@@ -1,14 +1,20 @@
 #ifndef _DECK_HPP_
 #define _DECK_HPP_
+#include <vector>
+#include "Card.hpp"
 class Deck
 {
 public:
-	
+	Deck()
+	{
+		std::vector<CardGame::Card> deck_ (smallDeck_);
+	}
 
 private:
+	const int bigDeck_ = 52;
+	const int smallDeck_ = 36;
 
-
-
+	//githab владислава - varkmort
 };
 
 #endif
--- Hand.hpp
+++ Hand.hpp
@@ -1,9 +1,15 @@
 #ifndef _HAND_HPP_
 #define _HAND_HPP_
+#include <vector>
+#include "Card.hpp"
 class Hand
 {
-
-
+public:
+	Hand()
+	{}
+	
+private:
+	std::vector<CardGame::Card> hand_;
 };
 
 
